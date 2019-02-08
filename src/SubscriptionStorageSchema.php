0c2713d716aff7e98e252a3be50ded3dfef13972
Alexei Skorobogatko <a.skorobogatko@soft-industry.com>
2019-02-08 17:24:12 +0200

    Namespaces.

--- src/SubscriptionStorageSchema.php
+++ src/SubscriptionStorageSchema.php
@@ -1,14 +1,19 @@
 <?php
 
+namespace Drupal\web_push_notification;
+
+use Drupal\Core\Entity\Sql\SqlContentEntityStorageSchema;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+
 /**
  * Defines the subscription entity schema handler.
  */
-class SubscriptionStorageSchema extends \Drupal\Core\Entity\Sql\SqlContentEntityStorageSchema {
+class SubscriptionStorageSchema extends SqlContentEntityStorageSchema {
 
   /**
    * {@inheritdoc}
    */
-  protected function getSharedTableFieldSchema(\Drupal\Core\Field\FieldStorageDefinitionInterface $storage_definition, $table_name, array $column_mapping) {
+  protected function getSharedTableFieldSchema(FieldStorageDefinitionInterface $storage_definition, $table_name, array $column_mapping) {
     $schema = parent::getSharedTableFieldSchema($storage_definition, $table_name, $column_mapping);
     $field_name = $storage_definition->getName();
 

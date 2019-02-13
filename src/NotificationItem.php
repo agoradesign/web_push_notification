30baa1e03e9cc4ad2b9f693ad9a94ca4fb5e56b9
Alexei Skorobogatko <a.skorobogatko@soft-industry.com>
2019-02-13 12:32:30 +0200

    Comments + refactoring.

--- src/NotificationItem.php
+++ src/NotificationItem.php
@@ -8,32 +8,61 @@ namespace Drupal\web_push_notification;
 class NotificationItem {
 
   /**
-   * @var array
+   * @var int[]
+   *   The list of Subscription entity ID.
    */
   public $ids = [];
 
   /**
    * @var string
+   *   The notification title.
    */
   public $title = '';
 
   /**
    * @var string
+   *   The notification message (body).
    */
-  public $message = '';
+  public $body = '';
 
-  public function __construct(string $title = '', string $message = '') {
+  /**
+   * @var string
+   *   The notification url.
+   */
+  public $url = '';
+
+  /**
+   * @var string
+   *  The notification image/icon.
+   */
+  public $icon = '';
+
+  /**
+   * NotificationItem constructor.
+   *
+   * @param string $title
+   *   The notification title.
+   *
+   * @param string $body
+   *   The notification message (body).
+   */
+  public function __construct(string $title = '', string $body = '') {
     $this->title = $title;
-    $this->message = $message;
+    $this->body = $body;
   }
 
   /**
+   * Converts the item to a web push payload.
+   *
    * @return string
+   *   A JSON encoded payload.
    */
   public function payload(): string {
     return json_encode([
       'title' => $this->title,
-      'message' => $this->message,
+      'body' => $this->body,
+      'url' => $this->url,
+      'icon' => $this->icon,
     ]);
   }
 }

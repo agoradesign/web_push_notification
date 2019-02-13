a531232b5665d7cc334a7a014ba61ee7e04ecfec
Alexei Skorobogatko <a.skorobogatko@soft-industry.com>
2019-02-13 12:38:27 +0200

    Service worker script.

--- js/service_worker.js
+++ js/service_worker.js
@@ -3,47 +3,45 @@
  * Serviceworker file for browser push notification.
  */
 
+self.addEventListener('install', function(event) {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('push', function (event) {
-  'use strict';
+
   if (!(self.Notification && self.Notification.permission === 'granted')) {
     return;
   }
-// console.log('push received');
-  const sendNotification = body => {
-    var str = body;
-    var message_array = str.split('<br>');
-    var notificationOptions = {
-      body: message_array[1],
-      icon: message_array[2],
-      badge: message_array[2],
-      tag: message_array[2],
-      data: {
-        url: message_array[3]
-      }
-    };
-    return self.registration.showNotification(message_array[0],
-            notificationOptions);
+
+  var data = {
+    body: '',
+    message: '',
+    icon: ''
   };
 
   if (event.data) {
-    const message = event.data.text();
-    event.waitUntil(sendNotification(message));
-  }
-});
-self.addEventListener('notificationclick', function (event) {
-  'use strict';
-  event.notification.close();
-  // console.log('notification click');
-  Promise.resolve();
-  if (event.notification.data && event.notification.data.url) {
-    clients.openWindow(event.notification.data.url);
+    data = event.data.json();
+    event.waitUntil(self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: data.icon
+    }));
   }
 });
 
-self.addEventListener('notificationclose', function (event) {
-  'use strict';
+self.addEventListener('notificationclick', function (event) {
   event.waitUntil(
-    Promise.all([
-    ])
+    self.clients.matchAll().then(function (clientList) {
+      if (clientList.length > 0) {
+        return clientList[0].focus();
+      }
+      if (event.data) {
+        var data = event.data.json();
+        return self.clients.openWindow(data.url);
+      }
+    })
   );
 });

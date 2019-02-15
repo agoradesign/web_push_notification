0d6266f78f5d3016abc7826efa42a260a615ea83
Alexei Skorobogatko <a.skorobogatko@soft-industry.com>
2019-02-15 12:16:59 +0200

    Create a notification from an entity.

--- src/NotificationQueue.php
+++ src/NotificationQueue.php
@@ -2,10 +2,13 @@
 
 namespace Drupal\web_push_notification;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Queue\QueueFactory;
-use Drupal\Core\Queue\QueueInterface;
+use Drupal\Core\Url;
+use Drupal\file\Plugin\Field\FieldType\FileFieldItemList;
 use Drupal\node\NodeInterface;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
 
 /**
  * Creates a queue for notification send.
@@ -13,9 +16,11 @@ use Drupal\node\NodeInterface;
 class NotificationQueue {
 
   /**
-   * @var \Drupal\Core\Queue\QueueFactory
+   * The web_push_queue queue object.
+   *
+   * @var \Drupal\Core\Queue\QueueInterface
    */
-  protected $queueFactory;
+  protected $queue;
 
   /**
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
@@ -23,26 +28,26 @@ class NotificationQueue {
   protected $entityManager;
 
   /**
+   * The web_push_notification config object.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $config;
+
+  /**
    * SendMessage constructor.
    *
    * @param \Drupal\Core\Queue\QueueFactory $queueFactory
-   *  The queue factory.
+   *   The queue factory.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityManager
-   *  The entity manager service.
+   *   The entity manager service.
+   * @param ConfigFactoryInterface $config_factory
+   *   The config factory service.
    */
-  public function __construct(QueueFactory $queueFactory, EntityTypeManagerInterface $entityManager) {
-    $this->queueFactory = $queueFactory;
+  public function __construct(QueueFactory $queueFactory, EntityTypeManagerInterface $entityManager, ConfigFactoryInterface $config_factory) {
+    $this->queue = $queueFactory->get('web_push_queue');
     $this->entityManager = $entityManager;
-  }
-
-  /**
-   * Returns the queue.
-   *
-   * @return \Drupal\Core\Queue\QueueInterface
-   *   The queue.
-   */
-  public function getQueue(): QueueInterface {
-    return $this->queueFactory->get('web_push_queue');
+    $this->config = $config_factory->get('web_push_notification.settings');
   }
 
   /**
@@ -52,8 +57,25 @@ class NotificationQueue {
    *   The node entity.
    */
   public function start(NodeInterface $entity) {
+    $bundle = $entity->bundle();
+    $fields = $this->config->get("fields.$bundle");
+
     $item = new NotificationItem();
     $item->title = $entity->getTitle();
+
+    $body = $entity->get($fields['body'])->value;
+    if (isset($fields['body']) && $body) {
+      $item->body = $this->prepareBody($body);
+    }
+
+    if (isset($fields['icon'])) {
+      $item->icon = $this->getIconUrl($entity->get($fields['icon']));
+    }
+
+    $item->url = $entity->toUrl('canonical', [
+      'absolute' => TRUE,
+    ])->toString();
+
     $this->startWithItem($item);
   }
 
@@ -67,13 +89,12 @@ class NotificationQueue {
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
   public function startWithItem(NotificationItem $baseItem) {
-    $queue = $this->getQueue();
     $query = $this->entityManager
       ->getStorage('wpn_subscription')
       ->getQuery();
 
     $start = 0;
-    $limit = $this->getProcessingLimit();
+    $limit = $this->config->get('queue_batch_size');
 
     while ($ids = $query->range($start, $limit)->execute()) {
       $item = new NotificationItem();
@@ -82,20 +103,47 @@ class NotificationQueue {
       $item->body = $baseItem->body;
       $item->icon = $baseItem->icon;
       $item->url = $baseItem->url;
-      $queue->createItem($item);
+      $this->queue->createItem($item);
       $start += $limit;
     }
   }
 
   /**
-   * Returns a queue processing limit.
+   * Returns an icon url from the entity field.
    *
-   * @return int
-   *   The queue processing limit.
+   * @param \Drupal\Core\Field\FieldItemList $field
+   *   The entity field.
+   *
+   * @return string
+   *   An icon url.
    */
-  protected function getProcessingLimit(): int {
-    return (int) \Drupal::config('web_push_notification.settings')
-      ->get('queue_batch_size');
+  protected function getIconUrl($field): string {
+    if ($field instanceof FileFieldItemList) {
+      if (!($entities = $field->referencedEntities())) {
+        return '';
+      }
+      $file = reset($entities);
+      return $file->url();
+    }
+    return '';
+  }
+
+  /**
+   * Prepares a notification body: trim and strip html tags.
+   *
+   * @param string $raw
+   *   The raw text.
+   *
+   * @return string
+   *   A trimmed and filtered text.
+   */
+  protected function prepareBody(string $raw): string {
+    $body = FieldPluginBase::trimText([
+      'max_length' => $this->config->get('body_length') ?: 100,
+      'html' => FALSE,
+    ], $raw);
+    $body = strip_tags($raw);
+    return $body;
   }
 
 }
